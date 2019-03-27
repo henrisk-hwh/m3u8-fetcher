079f3c030b2f83f4574ffaaeafecf4755ccba5ea
Your Name <you@example.com>
2019-03-27 02:14:36 -0700

    test #1
    Signed-off-by: Your Name <you@example.com>

--- fetch_m3u8.sh
+++ fetch_m3u8.sh
@@ -1,17 +1,73 @@
-#!/bin/sh
+#!/bin/bash
 #$1 url
+
+download() {
+    #$1 url
+    #$2 dst
+    #$3 rename
+    url=$1
+    target_file=${url##*/}
+    if [ $# -eq 1 ]; then
+        if [ -f $target_file ]; then
+            echo $target_file exist! skip $1
+            return
+        fi
+	    echo "download" $1
+        wget -q $1
+    fi
+
+    if [ $# -eq 2 ]; then
+        target_file=$2/$target_file
+        if [ -f $target_file ]; then
+            echo $target_file exist! skip $1
+            return
+        fi
+	    echo "download" $1 "--->" $2
+        wget -P $2 $1
+    fi
+
+    if [ $# -eq 3 ]; then
+        target_file=$2/$3
+        if [ -f $target_file ]; then
+            echo $target_file exist! skip $1
+            return
+        fi
+	    echo "download" $1 "--->" $2/$3
+        wget -q -P $2 $1 -O $3
+    fi
+}
+
 url=$1
 file=${url##*/}
+remote_file=remote_$file
 domain=${url%%$file*}
-echo fetch domain:$domain, file:$file
-
-rm -rf $file
-wget $url
-
 media_dir=./ts
-mkdir -p $media_dir
-cd $media_dir
-cat ../$file | grep http | xargs wget {}\;
-cd -
+local_file=local.m3u8
 
-change_remote_to_local.sh $file local.m3u8 $media_dir
+mkdir -p $media_dir
+
+echo fetch domain:$domain, file:$file, remote_file:$remote_file
+
+#fetch m3u8 file
+rm -rf $file $local_file $remote_file
+download $url . $remote_file
+
+download_url=""
+local_url=""
+
+while read line
+do
+	if [ ${line:0:1} != "#" ]; then
+		if [ ${line:0:4} == "http" ]; then
+            download_url=$line
+            local_url=$media_dir/${line##*/}
+		else
+            download_url=$domain/$line
+            local_url=$media_dir/$line
+		fi
+        echo $local_url >> $local_file
+        download $download_url $media_dir
+    else
+        echo $line >> $local_file
+	fi
+done  < $remote_file
